ed2299a6aea0db84817a28526d0fb2649a8fa824
discovercrew2026-del <discovercrew2026@gmail.com>
2026-06-16 11:34:47 +0530

    fix: suppress hydration warning caused by browser extensions

--- app/layout.jsx
+++ app/layout.jsx
@@ -44,7 +44,7 @@ const jsonLdSchema = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -57,7 +57,7 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSchema) }}
         />
       </head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
